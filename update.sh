e7b4f8730775ab85c72b11e3277222de658f8e95
Dimank <adiibnurohman23@gmail.com>
2026-01-05 15:53:47 +0700

    Fix wget URL to download menu.zip correctly

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://github.com/Dimank-cyber/TUNNELING/tree/main/menu/menu.zip
+    wget https://github.com/Dimank-cyber/TUNNELING/main/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
